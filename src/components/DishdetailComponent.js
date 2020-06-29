f990a7bad61edb277d0bfe533d786f63dd0be40b
Sushant Sanjay Sawant <sushantsawant865@gmail.com>
2020-06-29 19:52:27 +0530

    added header and footer

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+  function RenderDish({dish}) {
+    if(dish != null) {
+      console.log(dish)
+      return (
+        <Card className="col-12 col-md-5 m-1">
+          <CardImg width="100%" src={dish.image} alt={dish.name} />  
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </Card>
+      );
+    }
+    else {
+      return (
+        <div></div>
+      );
+    }
+  }
+
+  function RenderComments({comments}) {
+    if(comments != null) {
+        const Comments=comments.map(review => {
+        return (
+          <div key={review.id} class="col-xs-12 col-sm-12 col-md-5 m-1">
+            <Card>
+              <h4>Comments</h4>
+              <div className="row">
+              <ul className="list-unstyled" >
+                <li className="list-item">{review.comment}</li>
+                <li className="list-item">{review.author}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(review.date)))}</li>
+              </ul>
+              </div>
+              
+            </Card>
+          </div>
+
+        );
+      });
+      return Comments
+    }
+    else {
+      return (
+        <div></div>
+      );
+    }
+  }
+
+const DishDetail=(props)=>{
+  if(props.dish!=null){
+    return (
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish}/>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+        <RenderComments comments={props.dish.comments} />
+        </div>
+      </div>
+  );
+  }else{
+    return(
+      <div></div>
+    )
+  }
+   
+  }
+
+
+export default DishDetail;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
+import { DISHES } from '../shared/dishes';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+        selectedDish: null
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish : dishId});
+  }
+
+  render() {
+    console.log("main->render")
+    console.log(this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0])
+    return (
+      <div>
+        <Header />
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
