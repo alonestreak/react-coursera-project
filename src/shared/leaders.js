d280019d83d560075aeeb97ec94f35a1624ab900
Sushant Sanjay Sawant <sushantsawant865@gmail.com>
2020-06-30 13:25:05 +0530

    working on contanct-us component

--- src/shared/leaders.js
+++ src/shared/leaders.js
@@ -1,8 +1,8 @@
 export const LEADERS = [
     {
       id: 0,
-      name: 'Peter Pan',
-      image: '/assets/images/alberto.png',
+      name: 'Sushant Sawant',
+      image: '/assets/images/sushant.jpg',
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
@@ -10,8 +10,8 @@ export const LEADERS = [
     },
     {
       id: 1,
-      name: 'Dhanasekaran Witherspoon',
-      image: '/assets/images/alberto.png',
+      name: 'Sushant Sawant',
+      image: '/assets/images/sushant.jpg',
       designation: 'Chief Food Officer',
       abbr: 'CFO',
         featured: false,
@@ -19,8 +19,8 @@ export const LEADERS = [
     },
     {
       id: 2,
-      name: 'Agumbe Tang',
-      image: '/assets/images/alberto.png',
+      name: 'Sushant Sawant',
+      image: '/assets/images/sushant.jpg',
       designation: 'Chief Taste Officer',
       abbr: 'CTO',
         featured: false,
@@ -28,8 +28,8 @@ export const LEADERS = [
     },
     {
       id: 3,
-      name: 'Alberto Somayya',
-      image: '/assets/images/alberto.png',
+      name: 'Sushant Sawant',
+      image: '/assets/images/sushant.jpg',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
